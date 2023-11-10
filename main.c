a535118f7713e23b5baeadea96da35dcd4c68910
RAUL SILVA ALONSO <rsilva003@ikasle.ehu.eus>
2023-11-10 10:52:35 +0100

    Mejoras estrcutura de datos

--- main.c
+++ main.c
@@ -3,11 +3,24 @@
 #include <pthread.h>
 #include <unistd.h>
 
-// Estructura para representar un PCB (Proceso)
-typedef struct {
-    int pid; // Identificador del proceso
-    // Otras propiedades del proceso a medida que se amplía el simulador
-} PCB;
+// Estructura PCB
+struct PCB {
+    int PID;
+    // Otros campos relacionados con el PCB
+    struct PCB* siguiente;
+};
+
+// Estructura ProcessQueue
+struct ProcessQueue {
+    struct PCB* first;
+    struct PCB* last;
+};
+
+void initializeProcessQueue(struct ProcessQueue* lista){
+    lista->first = NULL;
+    lista->last = NULL;
+}
+
 
 // Estructura para representar la máquina (CPUs, cores, hilos)
 typedef struct {
@@ -50,23 +63,69 @@ void *process_generator_thread(void *args) {
     return NULL;
 }
 
+void addPCB(struct ProcessQueue* lista, struct PCB* pcb){
+    if(lista->first==NULL){
+        lista->first=pcb;
+        lista->last=pcb;
+    }else{
+        lista->last->siguiente=pcb;
+        lista->last=pcb;
+        }
+}
+
 int main(int argc, char *argv[]) {
+
+// Inicializar una cola de procesos con capacidad para 10 PCBs
+    // Crear una cola de procesos vacía
+    struct ProcessQueue myQueue;
+    initializeProcessQueue(&myQueue);
+
+    // Añadir dos nuevos PCB a la cola
+    struct PCB* pcb1 = (struct PCB*)malloc(sizeof(struct PCB));
+    pcb1->PID = 1;
+    pcb1->siguiente = NULL;
+    addPCB(&myQueue, pcb1);
+
+    struct PCB* pcb2 = (struct PCB*)malloc(sizeof(struct PCB));
+    pcb2->PID = 2;
+    pcb2->siguiente = NULL;
+    addPCB(&myQueue, pcb2);
+
+    // Puedes seguir añadiendo más PCBs según sea necesario
+
+    // Imprimir el contenido de la cola
+    struct PCB* current = myQueue.first;
+    while (current != NULL) {
+        printf("PID: %d\n", current->PID);
+        current = current->siguiente;
+    }
+
+    // Liberar memoria asignada para PCBs
+    current = myQueue.first;
+    while (current != NULL) {
+        struct PCB* temp = current;
+        current = current->siguiente;
+        free(temp);
+    }
+
+    return 0;
+
     // Parsear las opciones de configuración si es necesario
 
     // Inicializar las estructuras de datos que se van a utilizar
-    Machine machine;
+    // Machine machine;
     // Inicializar otras estructuras de datos a medida que se amplía el simulador
 
-    // Crear hilos para cada subsistema
-    pthread_t clock_tid, timer_tid, process_generator_tid;
-    pthread_create(&clock_tid, NULL, clock_thread, NULL);
-    pthread_create(&timer_tid, NULL, timer_thread, NULL);
-    pthread_create(&process_generator_tid, NULL, process_generator_thread, NULL);
+    // // Crear hilos para cada subsistema
+    // pthread_t clock_tid, timer_tid, process_generator_tid;
+    // pthread_create(&clock_tid, NULL, clock_thread, NULL);
+    // pthread_create(&timer_tid, NULL, timer_thread, NULL);
+    // pthread_create(&process_generator_tid, NULL, process_generator_thread, NULL);
 
-    // Esperar a que los hilos terminen (esto no debería suceder en un kernel real)
-    pthread_join(clock_tid, NULL);
-    pthread_join(timer_tid, NULL);
-    pthread_join(process_generator_tid, NULL);
+    // // Esperar a que los hilos terminen (esto no debería suceder en un kernel real)
+    // pthread_join(clock_tid, NULL);
+    // pthread_join(timer_tid, NULL);
+    // pthread_join(process_generator_tid, NULL);
 
     return 0;
 }
